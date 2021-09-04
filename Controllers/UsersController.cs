e16c206b38225b1ba3d547b2bfcac8b8f6248f81
Karina Andriushchenko <karina.andr@icloud.com>
2021-09-04 15:03:44 +0600

    Added entity user, registration page

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using ControlWork7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlWork7.Controllers
+{
+    public class UsersController : Controller
+    {
+        private MobileContext _context;
+        public UsersController(MobileContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (user != null)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- Models/MobileContext.cs
+++ Models/MobileContext.cs
@@ -9,6 +9,7 @@ namespace ControlWork7.Models
     public class MobileContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
         public MobileContext(DbContextOptions<MobileContext> options) : base(options) { }
     }
 }
