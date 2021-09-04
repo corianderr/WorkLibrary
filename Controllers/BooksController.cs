d33d40960b0b44284a778282931edbac4a9b72e7
Karina Andriushchenko <karina.andr@icloud.com>
2021-09-04 14:31:20 +0600

    Added 'index' and 'create' page

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using ControlWork7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlWork7.Controllers
+{
+    public class BooksController : Controller
+    {
+        private MobileContext _context;
+
+        public BooksController(MobileContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IQueryable<Book> books = _context.Books;
+            books = books.OrderByDescending(s => s.AddingDate);
+            return View(books.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (book != null)
+            {
+                book.AddingDate = DateTime.Now;
+                book.Status = "new";
+                _context.Books.Add(book);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
