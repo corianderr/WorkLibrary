9a12e7f5115cc593cceec770893dd676eddc41ed
Karina Andriushchenko <karina.andr@icloud.com>
2021-09-04 17:07:33 +0600

    Added 'BookAndEmail' view model

---
+++ ViewModels/BookAndEmail.cs
@@ -0,0 +1,15 @@
+﻿using ControlWork7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlWork7.ViewModels
+{
+    public class BookAndEmail
+    {
+        public Book Book { get; set; }
+        public string Email { get; set; }
+    }
+}
